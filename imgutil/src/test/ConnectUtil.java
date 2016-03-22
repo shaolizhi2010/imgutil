d1438c3d2c64b6fe7a81361e1964ea8484114082
lizhi <lizhi@like1>
2016-03-22 17:00:03 +0800

    1

---
+++ imgutil/src/test/ConnectUtil.java
@@ -0,0 +1,47 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class ConnectUtil {
+	
+	public static void getTitle(){
+		URL url;
+		try {
+			url = new URL("http://pan.baidu.com/share/link?shareid=1023176242&uk=1798788396");
+		
+
+			URLConnection connection = url.openConnection();
+		
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(),"utf-8"));
+			String line;
+			String result = "";
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+			in.close();
+			
+			String title = StringUtils.substringBetween(result, "<title>", "</title>");
+			
+			System.out.println(title);
+		
+		} catch ( Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		for(int i=0;i<100 ;i++){
+			getTitle();
+		}
+		
+	}
+}
